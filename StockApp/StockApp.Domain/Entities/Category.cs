472b9b3e5d2d9fc528c701899397c9e88843c2c9
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-03-18 12:40:36 -0300

    Aula1803-Validações restantes

--- StockApp/StockApp.Domain/Entities/Category.cs
+++ StockApp/StockApp.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockApp.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -12,6 +13,26 @@ namespace StockApp.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public Category(string name)
+        {
+            ValidationDomain(name);        
+        }
+
+        public Category(int id, string name) 
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid id value");
+            Id = id;
+            ValidationDomain(name);
+        }
+
         public ICollection<Product> Products { get; set;}
+
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name, name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short. Min 3 characters");
+            Name = name;    
+        }
     }
 }
--- StockApp/StockApp.Domain/Entities/Product.cs
+++ StockApp/StockApp.Domain/Entities/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StockApp.Domain.Validation;
 
 namespace StockApp.Domain.Entities
 {
@@ -16,6 +17,32 @@ namespace StockApp.Domain.Entities
         public string Image { get; set; }
 
         public int CategoryId { get; set; }
+
+        public Product()
+        {
+
+        }
         public Category Category { get; set; }
+
+        private void ValidateDomain(string name,  string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(name),
+                "Invalid name, name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minium 3 characters.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Invalid description, name is required.");
+
+            DomainExceptionValidation.When(description.Length < 5,
+                "Invalid description, too short, minimum 5 characters.");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price negative value.");
+
+            DomainExceptionValidation.When(stock < 0, "Invalid stock negative value.");
+
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, maximum 250 characters.");
+        }
     }
 }
