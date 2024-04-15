b33f39d5d38c9b5ed0eafea451922c890cb79aa1
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-04-15 12:28:21 -0300

    NovaBranch

---
+++ StockApp/StockApp.Domain.Test/CategoryUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Domain.Entities;
+using FluentAssertions;
+
+
+namespace StockApp.Domain.Test
+{
+    public class CategoryUnitTest
+    {
+        #region Teste Positivos
+        [Fact (DisplayName ="Create Category With Valid State")]
+        public void CreateCategory_WithValidParameters_ResultValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should().NotThrow<StockApp.Domain.Validation.DomainExceptionValidation>();
+        }
+        #endregion
+        #region Teste Negativos
+        [Fact(DisplayName ="Create Category With Invalid State Id")]
+        public void CreateCategory_WithInvalidParameters_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should().Throw<StockApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id Value.");
+        }
+        [Fact(DisplayName = "Create Category With Invalid State Name")]
+        public void CreateCategory_WithInvalidParameters_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should().Throw<StockApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters.");
+        }
+        [Fact(DisplayName = "Create Category With Null State Name")]
+        public void CreateCategory_WithNullParameters_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should().Throw<StockApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, name is required.");
+        }
+        #endregion
+    }
+}
---
+++ StockApp/StockApp.Domain.Test/ProductUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Domain.Entities;
+using FluentAssertions;
+
+namespace StockApp.Domain.Test
+{
+    public class ProductUnitTest
+    {
+        #region Testes Positivos
+        #endregion
+
+        #region Testes Negativos
+        #endregion
+    }
+}
