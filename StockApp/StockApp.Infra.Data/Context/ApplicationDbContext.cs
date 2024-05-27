df69e16c38e7cc8e693bbfb634743522d3d9aeff
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-05-27 12:35:59 -0300

    Aula2705 ok

--- StockApp/StockApp.Infra.Data/Context/ApplicationDbContext.cs
+++ StockApp/StockApp.Infra.Data/Context/ApplicationDbContext.cs
@@ -13,8 +13,15 @@ namespace StockApp.Infra.Data.Context
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
         { }
-        //sqlservericoma.database.windows.net
+        
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            
+        }   
     }
 }
